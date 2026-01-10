35a87424ca7dd21d2087b1491751c395dfc2cf81
Daniel <daniel.sofran@stud.ubbcluj.ro>
2026-01-10 22:25:19 +0200

    add load balancer

--- apps/api-gateway/src/app/auth.service.ts
+++ apps/api-gateway/src/app/auth.service.ts
@@ -76,7 +76,7 @@ export class AuthService {
   }
 
   async login(user: any): Promise<any> {
-    const payload = { email: user.email, sub: user.id }
+    const payload = { email: user.email, sub: user.id, role: user.role }
     return {
       access_token: this.jwtService.sign(payload),
     }
